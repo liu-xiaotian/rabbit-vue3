d48a07742b07ac1bb794adaa2f54dbcbae34a221
tt <xiaotian3303@outlook.com>
2025-12-06 17:57:54 +0800

    layout-渲染一级导航

--- src/main.js
+++ src/main.js
@@ -7,14 +7,7 @@ import App from './App.vue'
 import router from './router'
 
 // 引入初始化样式文件
-import '@/styles/common.scss'
-
-// 测试接口函数
-import { getCategory } from '@/apis/testAPI';
-getCategory().then(res=>{
-  console.log(res);
-  
-})
+import '@/styles/common.scss' 
 
 const app = createApp(App)
 
