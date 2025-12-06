98155b09f0014973fecff2e1cc126c10095b3131
tt <xiaotian3303@outlook.com>
2025-12-06 15:51:56 +0800

    路由设计

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import App from './App.vue'
 import router from './router'
 
 // 测试接口函数
-import { getCategory } from '@/apis/testapi';
+import { getCategory } from '@/apis/testAPI';
 getCategory().then(res=>{
   console.log(res);
   
---
+++ src/views/Login/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    我是登录
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
