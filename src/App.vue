719945831f0128f760a3f3942f1042ff567ce2aa
tt <xiaotian3303@outlook.com>
2025-12-06 16:10:35 +0800

    sass 文件自动导入

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,11 @@
 <template>
   <!-- 一级路由出口 -->
   <RouterView />
+  <div class="test">123 sass</div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.test{
+  color:$priceColor
+}
 </style>
