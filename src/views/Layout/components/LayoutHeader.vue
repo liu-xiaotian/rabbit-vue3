9b96f8f557d020a42ef841904646dabc9fb441f9
tt <xiaotian3303@outlook.com>
2025-12-06 18:35:46 +0800

    Pinia 优化重复代码

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,17 +1,9 @@
 <script setup>
-import { getCategoryAPI } from '@/apis/layout';
-import { onMounted,ref } from 'vue';
+import {useCategoryStore} from '@/stores/category'
 
-const categoryList = ref([])
-const getCategory = async () =>{
-  const res = await getCategoryAPI()
-  console.log(res);
-   categoryList.value = res.result
-}
+// 使用 pinia数据
+const categoryStore = useCategoryStore()
 
-onMounted(()=>{
-  getCategory()
-})
 </script>
 
 <template>
@@ -24,7 +16,7 @@ onMounted(()=>{
         <li class="home">
           <RouterLink to="/">首页</RouterLink>
         </li>
-        <li v-for="item in categoryList" :key="item.id"> <RouterLink to="/">{{item.name}}</RouterLink> </li>
+        <li v-for="item in categoryStore.categoryList" :key="item.id"> <RouterLink to="/">{{item.name}}</RouterLink> </li>
       </ul>
       <div class="search">
         <i class="iconfont icon-search"></i>
