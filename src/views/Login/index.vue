c25eaa7bce3e7cab6c2cfa565e780dbba52a825d
tt <xiaotian3303@outlook.com>
2025-12-07 21:03:59 +0800

    登录-校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,28 @@
 <script setup>
+import { ref } from 'vue';
 
+const userInfo = ref({
+  account:'111111',
+  password:'123456',
+  agrre:true
+})
+
+const rules={
+  account:[
+    {require:true, message:'用户名不能为空'}
+  ],
+  password:[
+    {require:true, message:'密码不能为空'},
+    {min:6, max:24, message: '密码长度要求6-24个字符'}
+  ],
+  agree:[
+    {
+      validator:(rule, val, callback)=>{
+        return val ? callback() : new Error('请先同意协议')
+      }
+    }
+  ]
+}
 </script>
 
 
@@ -24,16 +47,16 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px"
+            <el-form label-position="right" label-width="60px" :model="userInfo" :rules="rules"
               status-icon>
-              <el-form-item  label="账户">
-                <el-input/>
+              <el-form-item prop="account" label="账户">
+                <el-input v-model="userInfo.account"/>
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input/>
+              <el-form-item prop="password" label="密码">
+                <el-input v-model="userInfo.password"/>
               </el-form-item>
               <el-form-item label-width="22px">
-                <el-checkbox  size="large">
+                <el-checkbox v-model="userInfo.agrre"  size="large">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
