9176577ac81654b3a2128fb94f231d633de5782c
tt <xiaotian3303@outlook.com>
2025-12-07 19:04:59 +0800

    详情热榜-周热榜和渲染

--- src/views/Detail/components/DetailHot.vue
+++ src/views/Detail/components/DetailHot.vue
@@ -1,13 +1,24 @@
 <script setup>
 import { getHotGoodsAPI } from '@/apis/detail';
-import { onMounted,ref } from 'vue';
+import { computed, onMounted,ref } from 'vue';
 import { useRoute } from 'vue-router';
 const goodList = ref([])
 const route = useRoute()
+const props = defineProps({
+  type:{
+    type: Number, // 1代表24小时热销榜 2代表周热销榜 3代表总热销榜 可以使用type去适配title和数据列表
+    default: 1
+  }
+})
+const TITLEMAP = {
+  1: '24小时热榜',
+  2: '周热榜', 
+}
+const title = computed(()=>TITLEMAP[props.type])
 const getHotList = async () => {
   const res = await getHotGoodsAPI({
     id: route.params.id,
-    type: 1
+    type: props.type
   })
   goodList.value = res.result
 }
@@ -18,7 +29,7 @@ onMounted(()=> getHotList())
 
 <template>
   <div class="goods-hot">
-    <h3> 24小时热榜 </h3>
+    <h3>{{ title }}</h3>
     <!-- 商品区块 -->
     <RouterLink :to="`/detail/${item.id}`" class="goods-item" v-for="item in goodList" :key="item.id">
       <img :src="item.picture" alt="" />
--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -117,10 +117,10 @@ onMounted(()=> getGoods())
             </div>
             <!-- 24热榜+专题推荐 -->
             <div class="goods-aside">
-              <!-- 24小时 -->
-              <DetailHot/>
-              <!-- 周 -->
-              <DetailHot/>
+              <!-- 24小时热榜 -->
+              <DetailHot :type="1" />
+              <!-- 周热榜 -->
+              <DetailHot :type="2" />
             </div>
           </div>
         </div>
