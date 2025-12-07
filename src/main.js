7aee99a406ecb23b1871df52af8eaa2cd57a009b
tt <xiaotian3303@outlook.com>
2025-12-07 13:49:23 +0800

    home-图片懒加载实现

--- src/main.js
+++ src/main.js
@@ -9,9 +9,27 @@ import router from './router'
 // 引入初始化样式文件
 import '@/styles/common.scss' 
 
+import { useIntersectionObserver } from '@vueuse/core'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+// 定义全局指令
+
+app.directive('img-lazy',{
+  mounted(el, binding){
+    useIntersectionObserver(
+      el,
+      ([{isIntersecting}]) => {
+        if(isIntersecting){
+          el.src = binding.value
+        }
+        
+      },
+    )
+  }
+})
