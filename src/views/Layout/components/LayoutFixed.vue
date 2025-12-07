332b52aec2f6f1ba1d66212b0c6616b5c2df511a
tt <xiaotian3303@outlook.com>
2025-12-07 14:35:16 +0800

    home-一级分类路由配置

--- src/views/Layout/components/LayoutFixed.vue
+++ src/views/Layout/components/LayoutFixed.vue
@@ -19,7 +19,7 @@ const categoryStore = useCategoryStore()
         <li class="home">
           <RouterLink to="/">首页</RouterLink>
         </li>
-        <li v-for="item in categoryStore.categoryList" :key="item.id"> <RouterLink to="/">{{item.name}}</RouterLink> </li>
+        <li v-for="item in categoryStore.categoryList" :key="item.id"> <RouterLink :to="`/category/${item.id}`">{{item.name}}</RouterLink> </li>
       </ul>
       <div class="right">
         <RouterLink to="/">品牌</RouterLink>
