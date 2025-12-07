a0e290d6f031e1edc45646b30e8a35a28402d4cc
tt <xiaotian3303@outlook.com>
2025-12-07 12:46:08 +0800

    home-面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
+import HomePanel from '@/views/Home/components/HomePanel.vue';
 </script>
 
 <template>
@@ -13,5 +14,8 @@ import homeProduct from './components/HomeProduct.vue'
   </div>
   <HomeNew />
   <HomeHot />
-  <homeProduct />
+  <homeProduct /> 
+  <HomePanel title="新鲜好物" subTitle="新鲜好物，人气推荐">
+    <div>123我是新鲜好物插槽</div>
+  </HomePanel>
 </template>
