b3548087da44f835245cbcbbf86857152f68f634
tt <xiaotian3303@outlook.com>
2025-12-07 13:16:17 +0800

    home-新鲜好物人气推荐实现

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,7 +4,6 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
-import HomePanel from '@/views/Home/components/HomePanel.vue';
 </script>
 
 <template>
@@ -14,8 +13,5 @@ import HomePanel from '@/views/Home/components/HomePanel.vue';
   </div>
   <HomeNew />
   <HomeHot />
-  <homeProduct /> 
-  <HomePanel title="新鲜好物" subTitle="新鲜好物，人气推荐">
-    <div>123我是新鲜好物插槽</div>
-  </HomePanel>
+  <homeProduct />
 </template>
