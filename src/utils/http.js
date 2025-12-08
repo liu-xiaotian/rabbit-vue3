1c57d56e9cdcdcf1f37c57f010abc7011fec2fe3
tt <xiaotian3303@outlook.com>
2025-12-08 15:21:33 +0800

    登录-请求拦截器配置token

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,7 @@
 import axios from 'axios';
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
+import { useUserStore } from '@/stores/user';
 
 const httpInstance = axios.create({
   baseURL:'http://pcapi-xiaotuxian-front-devtest.itheima.net/',
@@ -12,6 +13,11 @@ const httpInstance = axios.create({
 
 // axios 请求拦截器
 httpInstance.interceptors.request.use(config=>{
+  const useStore = useUserStore()
+  const token = useStore.userInfo.token
+  if(token){
+    config.headers.Authorization = `Bearer ${token}`
+  }
   return config
 },e => Promise.reject(e))
 
