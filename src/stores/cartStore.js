c248e1a3fa9b96e404f51db41759af3e2c23014e
tt <xiaotian3303@outlook.com>
2025-12-08 17:37:57 +0800

    本地购物车-头部购物车删除

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -17,8 +17,15 @@ export const useCartStore = defineStore('cart', ()=>{
         cartList.value.push(goods)
       }
     }
+    const delCart = (skuId)=>{
+      //1. 找到要删除的下标值 -splice
+      //2. 使用数组的过滤方法 - filter
+      const idx = cartList.value.findIndex((item)=> skuId === item.skuId)
+      cartList.value.splice(idx, 1)
+    }
     return{
       cartList,
-      addCart
+      addCart,
+      delCart
     }
   })
