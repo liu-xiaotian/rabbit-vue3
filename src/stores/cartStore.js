a761bfef25493e15d5c79df6bdd3087cbbc6ff79
tt <xiaotian3303@outlook.com>
2025-12-09 14:31:28 +0800

    本地购物车-单选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -23,6 +23,11 @@ export const useCartStore = defineStore('cart', ()=>{
       const idx = cartList.value.findIndex((item)=> skuId === item.skuId)
       cartList.value.splice(idx, 1)
     }
+    // 单选功能
+    const singleCheck = (skuId, selected)=>{
+      const item = cartList.value.find((item) => item.skuId === skuId)
+      item.selected = selected
+    }
     // 计算属性
     const allCount = computed(()=> cartList.value.reduce((a,c) => a+c.count, 0))
     const allPrice = computed(() => cartList.value.reduce((a,c) => a+c.count*c.price,0))
@@ -32,6 +37,7 @@ export const useCartStore = defineStore('cart', ()=>{
       allCount,
       allPrice,
       addCart,
-      delCart
+      delCart,
+      singleCheck
     }
   })
